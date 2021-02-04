7aea4a3d0e7b40949ab296aa600194e9748b75f1
Michael Zhu <michaelzhu63@hotmail.com>
2021-02-04 17:48:09 -0500

    Implemented type effect

---
+++ js/typed.js
@@ -0,0 +1,13 @@
+// import Typed from 'typed.js';
+
+const options = {
+    strings: ["Hey, I’m Michael Zhu and I love building beautiful websites."],
+    typeSpeed: 50,
+    smartBackspace: true,
+    loopCount: Infinity,
+    fadeOut: true,
+    cursorChar: "",
+    // loop: true
+};
+
+const typed = new Typed ('#intro-typing-effect', options);
